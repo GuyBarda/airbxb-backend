d2ac46b6d6f2bc89501685dca43193f84a5cc39f
Guy Barda <guy.barda.1234@gmail.com>
2022-12-07 00:52:45 +0200

    first commit

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    dbURL: 'mongodb://127.0.0.1:27017',
+    dbName: 'stay_db',
+    // dbURL: 'mongodb+srv://airbxb:gg1@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+    // dbName: 'airbxb_db',
+};
