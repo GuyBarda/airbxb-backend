c79afec833973c519e7da21a4e157f1d7417e677
Guy Barda <guy.barda.1234@gmail.com>
2022-12-11 11:28:28 +0200

    filter working type

--- api/stay/stay.service.js
+++ api/stay/stay.service.js
@@ -50,14 +50,14 @@ async function add(stay) {
 
 async function update(stay) {
     try {
-        const stayToSave = JSON.parse(JSON.stringify(stay))
-        delete stayToSave._id
+        const stayToSave = JSON.parse(JSON.stringify(stay));
+        delete stayToSave._id;
         const collection = await dbService.getCollection('stay');
 
         await collection.updateOne(
             { _id: ObjectId(stay._id) },
             { $set: stayToSave }
-            );
+        );
         return stay;
     } catch (err) {
         logger.error(`cannot update stay ${stayId}`, err);
@@ -109,12 +109,7 @@ function _buildCriteria({
     beds,
 }) {
     const criteria = {};
-    console.log(
-        name,
-        type,
-        amenities,
-        destination
-    );
+
     if (name) {
         criteria.name = { $regex: name, $options: 'i' };
     }
@@ -141,8 +136,8 @@ function _buildCriteria({
         criteria['loc.country'] = { $regex: destination, $options: 'i' };
     }
 
-    if (propertyTypes) {
-        criteria.propertyType = { $in: propertyTypes.split(',') };
+    if (propertyTypes && propertyTypes.length) {
+        criteria.proprtyType = { $in: propertyTypes.split(',') };
     }
 
     if (guests) {
@@ -160,6 +155,7 @@ function _buildCriteria({
     if (beds) {
         criteria.beds = { $gte: +beds };
     }
+    console.log(criteria);
     return criteria;
 }
 
